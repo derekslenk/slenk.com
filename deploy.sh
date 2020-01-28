bf08b5579971f76faca977d559cc4511ecd7ce06
Derek Slenk <derek.slenk@gmail.com>
2020-01-27 22:57:43 -0500

    Simple deploy script

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Deleting old files"
+
+rm -rf public
+
+echo "Generating Files"
+
+hugo --minify
+
+echo "Deploying to S3 and Invalidating CDN"
+
+hugo deploy --maxDeletes -1 --invalidateCDN
+
+echo "Deployment Completed"
